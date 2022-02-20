eab99940c799be8c851c9abce7dd96714ed98b0e
Andrew <korotkov915@gmail.com>
2022-02-20 16:54:22 +0300

    ADD: validation forms

--- source/js/main.js
+++ source/js/main.js
@@ -153,3 +153,78 @@ const openModal = (button) => {
 
 openModal(buttonBuyListTrip);
 openModal(buttonBuyListPrice);
+
+// Валидация формы
+
+// Зададим регулярные выражения для проверки ввода данных пользователем в поля ввода
+const validityEmail = /^[A-Za-z0-9]{1,19}@[A-Za-z0-9]{1,19}.[rsucomwinf]{1,8}$/;
+const validityInputNumber = /^[0-9]{1,20}$/;
+const formQuestions = document.querySelector(".questions__form");
+const inputPhone = formQuestions.querySelector(".form__input--phone");
+const inputEmail = formQuestions.querySelector(".form__input--email");
+const formModal = document.querySelector(".buy-trip__form");
+const inputPhoneModal = formModal.querySelector(".form__input--phone");
+const inputEmailModal = formModal.querySelector(".form__input--email");
+const MIN_LENGTH_PHONE = 5;
+const MAX_LENGTH_PHONE = 10;
+
+const onInputValueMissing = (evt) => {
+  const field = evt.target;
+  if (field.validity.valueMissing) {
+    field.setCustomValidity('Заполните обязательное поле.');
+    setFormError(field);
+  }
+};
+
+const onInputValueError = (evt) => {
+  const field = evt.target;
+  setFormError(field);
+}
+
+const setFormError = (nameInput) => {
+  nameInput.classList.add("form__input--invalid");
+  nameInput.previousElementSibling.classList.add("form__invalid--active");
+};
+
+const setFormValidityOk = (evt) => {
+  const field = evt.target;
+  field.previousElementSibling.classList.remove("form__invalid--active");
+  field.classList.remove("form__input--invalid");
+};
+
+const validation = (namePhone, nameEmail) => {
+  namePhone.addEventListener("input", (evt) => {
+    const lengthInputPhone = namePhone.value.length;
+
+    if (!validityInputNumber.test(namePhone.value)) {
+      onInputValueError(evt)
+      namePhone.setCustomValidity(`Номера телефона должен содержать только цыфры`);
+    } else if (lengthInputPhone < MIN_LENGTH_PHONE) {
+      onInputValueError(evt)
+      namePhone.setCustomValidity(`Еще нужно ввести минимум ${MIN_LENGTH_PHONE - lengthInputPhone} цыфр`);
+    } else if (lengthInputPhone >  MAX_LENGTH_PHONE) {
+      onInputValueError(evt)
+      namePhone.setCustomValidity(`Нужно удалить ${lengthInputPhone - MAX_LENGTH_PHONE} цыфр`);
+    } else {
+      namePhone.setCustomValidity("");
+      setFormValidityOk(evt);
+    };
+    namePhone.reportValidity();
+  });
+
+  nameEmail.addEventListener("input", (evt) => {
+    if (!validityEmail.test(nameEmail.value)) {
+      onInputValueError(evt)
+      nameEmail.setCustomValidity(`Введите корректный Email`);
+    } else {
+      nameEmail.setCustomValidity("");
+      setFormValidityOk(evt);
+    };
+    nameEmail.reportValidity();
+  });
+
+  namePhone.addEventListener('invalid', onInputValueMissing, true);
+};
+
+validation(inputPhone, inputEmail);
+validation(inputPhoneModal, inputEmailModal);
